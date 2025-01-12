c5681d8232933e1cae2159cd53f89134be649558
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-01-12 15:45:15 +0900

    [2025-01-08]: HTTP와 백그라운드 태스크

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,10 +1,18 @@
 package com.bignerdranch.android.photogallery.api
 
+import com.bignerdranch.android.photogallery.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
 
+private const val apiKey = BuildConfig.MY_API
+
 interface FlickrApi {
 
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=$apiKey" +
+        "&format=json" +
+        "&nojsoncallback=1" +
+        "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
 }
