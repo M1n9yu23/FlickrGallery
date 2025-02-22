c6ded5373ebb0f640e4f9cbc47927f96abac3034
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-22 14:56:58 +0900

    [chore]: GalleryItem
    필요한 속성 추가

--- app/src/main/java/com/bossmg/android/flickrgallery/data/GalleryItem.kt
+++ app/src/main/java/com/bossmg/android/flickrgallery/data/GalleryItem.kt
@@ -9,14 +9,23 @@ data class GalleryItem(
     @SerializedName("url_s")
     var url: String = "",
     @SerializedName("owner")
-    var owner: String = ""
+    var owner: String = "", // 작성자 고유 ID
+    @SerializedName("ownername")
+    var ownerName: String = "", // 작성자 이름
+    @SerializedName("dateupload")
+    var dateUpload: String = "", // 업로드 날짜
+    @SerializedName("datetaken")
+    var dateTaken: String = "", // 촬영 날짜
+    @SerializedName("tags")
+    var tags: String = "" // 사진 태그
 ) {
+    // 사진 상세 페이지 URI 생성
     val photoPageUri: Uri
         get() {
             return Uri.parse("https://www.flickr.com/photos/")
                 .buildUpon()
-                .appendPath(owner)
-                .appendPath(id)
+                .appendPath(owner) // 작성자의 고유 ID
+                .appendPath(id) // 사진 ID
                 .build()
         }
 }
